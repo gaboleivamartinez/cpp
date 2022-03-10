4ef0fe340eb76379e31da38ae554c8cfc5336920
gaboleivamartinez <98678710+gaboleivamartinez@users.noreply.github.com>
2022-03-10 00:43:01 -0600

    Add files via upload

---
+++ Ejercicios/Ejercicio35-Punto-de-venta-TAREA/Productos.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+
+using namespace std;
+
+void productos(int opcion);
+
+extern void  agregarProducto(string descripcion, int cantidad,double precio);
+void productos(int opcion)
+{
+	system ("cls");
+	
+	int opcionProducto= 0;
+	
+	switch (opcion)
+	{
+		case 1:
+		
+	    cout<<"BEBIDAS CALIENTES"<<endl;
+		cout<<"************"<<endl;
+		cout<<"1.Capuchino"<<endl;
+		cout<<"2.Expreso"<<endl;
+		cin>>opcionProducto;
+		switch (opcionProducto)
+			{
+				case 1: agregarProducto("1 Capuchino - L 40.00",1,40);
+				break;
+				case 2: agregarProducto("2 Expreso - L 30.00",1,30);
+				break;
+				default:
+					{
+					cout<< "Opcion no valida";
+					return;
+					break;
+					}
+			}
+			cout<<endl;
+			cout<<"Producto agregado"<<endl<<endl;
+			system("pause");
+			
+		break;
+		case 2: cout<<"BEBIDAS FRIAS"<<endl;
+		cout<<"************"<<endl;
+		cout<<"1.Te Frio"<<endl;
+		cout<<"2.Coca Cola"<<endl;
+		cin>>opcionProducto;
+		switch (opcionProducto)
+			{
+				case 1: agregarProducto("1 Te lipton - L 25.00",1,25);
+				break;
+				case 2: agregarProducto("2 Coca Cola - L 20.00 ",1,20);
+				break;
+				default:
+					{
+					cout<< "Opcion no valida";
+					return;
+					break;
+					}
+			}
+			cout<<endl;
+			cout<<"Producto agregado"<<endl<<endl;
+		system("pause");
+		break;
+		
+		case 3: cout<<"REPOSTERIA"<<endl;
+		cout<<"************"<<endl;
+		cout<<"1.Chilena"<<endl;
+		cout<<"2.Tres leches"<<endl;
+		cin>>opcionProducto;
+		switch (opcionProducto)
+			{
+				case 1: agregarProducto("1 Chilena - L 20.00",1,20);
+				break;
+				case 2: agregarProducto("2 tres leches - L 40.00",1,40);
+				break;
+				default:
+					{
+					cout<< "Opcion no valida";
+					return;
+					break;
+					}
+			}
+			cout<<endl;
+			cout<<"Producto agregado"<<endl<<endl;
+		system("pause");
+		break;
+		
+		default :
+			break;
+		
+	}
+}
---
+++ Ejercicios/Ejercicio35-Punto-de-venta-TAREA/factura.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+using namespace std;
+
+double subtotal;
+float impuesto=0,totalPagar=0;
+
+string listaProductos;
+
+void agregarProducto(string descripcion, int cantidad,double precio)
+{
+	listaProductos=listaProductos+descripcion+ '\n';
+	subtotal= subtotal + (cantidad * precio);
+	impuesto=subtotal*0.15;
+	totalPagar=subtotal+impuesto;
+}
+
+extern void imprimirFactura()
+{
+	system("cls");
+	cout<<"******"<<endl;
+	cout<<"FACTURA"<<endl;
+	cout<<"******"<<endl;
+	
+	cout<<"Productos: "<<endl;
+	cout<<listaProductos;
+	
+	cout<<endl;
+	
+	cout<<"Subtotal: "<<subtotal<<endl;
+	cout<<"Impuesto 15%: "<<impuesto<<endl;
+	cout<<"TOTAL A PAGAR ES: "<<totalPagar<<endl;
+	cout<<endl;
+	cout<<endl;
+	system("pause");
+	
+}
