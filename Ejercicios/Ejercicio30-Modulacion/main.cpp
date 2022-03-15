9dd66536f6c4d33cfc7867dec7f67579b1241871
gaboleivamartinez <98678710+gaboleivamartinez@users.noreply.github.com>
2022-03-15 10:49:12 -0600

    Add files via upload

---
+++ Ejercicios/Ejercicio30-Modulacion/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+extern int sumar(int a, int b);
+
+int main(int argc, char** argv) {
+	system("cls");
+	
+	cout<<sumar(5,7);
+	cout<<endl;
+	
+	return 0;
+}
