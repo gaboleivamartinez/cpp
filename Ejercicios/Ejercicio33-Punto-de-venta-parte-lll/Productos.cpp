8e511c241f2dd0e6d4f052d29c7d84d440959098
gaboleivamartinez <98678710+gaboleivamartinez@users.noreply.github.com>
2022-03-15 10:52:39 -0600

    Add files via upload

---
+++ Ejercicios/Ejercicio33-Punto-de-venta-parte-lll/Productos.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+
+using namespace std;
+
+void productos(int opcion);
+
+extern void  agregarProducto(string descripcion, int cantidad,double precio);
+void productos(int opcion)
+{
+	system ("cls");
+	
+	int opcionProducto= 0;
+	
+	switch (opcion)
+	{
+		case 1:
+		
+	    cout<<"BEBIDAS CALIENTES"<<endl;
+		cout<<"************"<<endl;
+		cout<<"1.Capuchino"<<endl;
+		cout<<"2.Expreso"<<endl;
+		cin>>opcionProducto;
+		switch (opcionProducto)
+			{
+				case 1: agregarProducto("Capuchino",1,40);
+				break;
+				case 2: agregarProducto("Capuchino",1,30);
+				break;
+				default:
+					cout<< "Opcion no valida";
+					break;
+				
+			}
+		break;
+		case 2: cout<<"BEBIDAS FRIAS"<<endl;
+		cout<<"************"<<endl;
+		system("pause");
+		break;
+		
+		case 3: cout<<"REPOSTERIA"<<endl;
+		cout<<"************"<<endl;
+		system("pause");
+		break;
+		
+		default :
+			break;
+		
+	}
+}
