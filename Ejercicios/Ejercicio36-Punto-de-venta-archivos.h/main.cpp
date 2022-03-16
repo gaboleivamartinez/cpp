9b3d3a10276af1305e1f9e078f6d31ff5eb9e17e
gaboleivamartinez <98678710+gaboleivamartinez@users.noreply.github.com>
2022-03-15 23:41:24 -0600

    Add files via upload

---
+++ Ejercicios/Ejercicio36-Punto-de-venta-archivos.h/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "menu.h"
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+using namespace std;
+
+ void menu();
+
+
+int main(int argc, char** argv) {
+	
+	menu();
+	
+	return 0;
+}
